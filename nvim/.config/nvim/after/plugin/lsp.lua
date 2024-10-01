07456a7b481eee277b2db18e4c8d666b6ac75a24
yaraikun <evan.calupig@gmail.com>
2024-10-01 14:22:04 +0800

    Added rust analyzer to lsp.lua in nvim config. Fixed path configuration in sessionizer script.

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -41,7 +41,7 @@ cmp.setup({
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { 'lua_ls', 'clangd' }
+	ensure_installed = { 'lua_ls', 'clangd', 'rust_analyzer'}
 })
 require("lspconfig").lua_ls.setup {
 	settings = {
@@ -53,4 +53,6 @@ require("lspconfig").lua_ls.setup {
 	}
 }
 require("lspconfig").clangd.setup {
+}
+require("lspconfig").rust_analyzer.setup {
 }
