f673f744a470ecefd50c91dd0338e8662f98e778
yaraikun <evan.calupig@gmail.com>
2025-05-20 15:46:13 +0800

    feat: added manual jdtls initialization

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -2,5 +2,4 @@ local config = {
     cmd = { "/opt/homebrew/opt/jdtls/bin/jdtls" },
     root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
-
 require("jdtls").start_or_attach(config)
