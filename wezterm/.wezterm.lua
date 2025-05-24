1dc05a7e7c5d0f790e866d4d6e8414a8943bd5cd
yaraikun <evan.calupig@gmail.com>
2025-05-25 00:08:26 +0800

    feat: removed padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,17 +1,24 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.window_decorations = "RESIZE"
-config.color_scheme = 'rose-pine'
--- config.window_background_opacity = 0.85
--- config.window_background_opacity = 0.90
-config.window_background_opacity = 0.95
--- config.window_background_opacity = 1
+-- COLORS
 config.colors = {
     background = "black",
 }
-config.enable_tab_bar = false
+config.color_scheme = 'rose-pine'
 
+-- APPEARANCE
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- FONT
 config.font = wezterm.font 'Fira Code'
 config.font_size = 24.0
 
