ef1c5b391b002c5d17790d3db4a435e49d3a8e74
yaraikun <evan.calupig@gmail.com>
2025-08-31 01:45:06 +0800

    feat: removed oil for now

--- nvim/.config/nvim/lua/yarai/oil.lua
+++ nvim/.config/nvim/lua/yarai/oil.lua
@@ -1,20 +1,20 @@
 return {
-    "stevearc/oil.nvim",
-    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-
-    config = function()
-        require("oil").setup {
-            -- columns = {"icon"},
-            keymaps = {
-                ["<C-p>"] = false,
-                ["<C-c>"] = false,
-            },
-            view_options = {
-                show_hidden = true,
-            },
-        }
-
-        vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
-        -- vim.keymap.set("n", "-", "<CMD>Oil<CR>")
-    end,
+    -- "stevearc/oil.nvim",
+    -- -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    --
+    -- config = function()
+    --     require("oil").setup {
+    --         -- columns = {"icon"},
+    --         keymaps = {
+    --             ["<C-p>"] = false,
+    --             ["<C-c>"] = false,
+    --         },
+    --         view_options = {
+    --             show_hidden = true,
+    --         },
+    --     }
+    --
+    --     vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
+    --     -- vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+    -- end,
 }
