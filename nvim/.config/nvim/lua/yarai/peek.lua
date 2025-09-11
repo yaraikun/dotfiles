6bc8e46726d263575490e7b27f03e41077a4db73
yaraikun <evan.calupig@gmail.com>
2025-09-11 12:05:56 +0800

    fix: peek
    Have to run the deno command manually in the share/nvim directory

--- nvim/.config/nvim/lua/yarai/peek.lua
+++ nvim/.config/nvim/lua/yarai/peek.lua
@@ -3,10 +3,28 @@ return {
         "toppair/peek.nvim",
         event = { "VeryLazy" },
         build = "deno task --quiet build:fast",
+
+        keys = {
+            { "<leader>md", function() require("peek").open() end, desc = "Peek (Markdown Preview)" },
+            { "<leader>mc", function() require("peek").close() end, desc = "Peek Close" },
+        },
+
         config = function()
             require("peek").setup()
             vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
             vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+
+            require('peek').setup({
+                auto_load = true,
+                close_on_bdelete = true,
+                syntax = true,
+                theme = 'dark',
+                update_on_change = true,
+                app = 'webview',
+                filetype = { 'markdown' },
+                throttle_at = 200000,
+                throttle_time = 'auto',
+            })
         end,
     },
 }
